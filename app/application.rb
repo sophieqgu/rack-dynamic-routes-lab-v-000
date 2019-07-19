2268d04b2894965726257f261d6ab111440bf622
Sophie Gu <sophieqgu@gmail.com>
2019-07-19 03:40:44 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,10 +1,11 @@
 class Application
   
+  @@items = []
+  
   def call(env)
     resp = Rack::Response.new 
     req = Rack::Request.new(env) 
     
-    @@items = []
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last 
       item = @@items.find{|i| i.name == item_name}
